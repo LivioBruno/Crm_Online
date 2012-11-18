0ab01b5ac6a5bb4d5e56b18605bac1d32b8c1c95
Livio Bruno <bruno_osprimos@hotmail.com>
2012-11-18 13:10:55 -0200

    Adicionado o teste para enviar o lembrete um dia antes da data da atividade

--- EnviaLembreteTest.java
+++ EnviaLembreteTest.java
@@ -1,5 +1,6 @@
 package TestClass;
 import Model.Email;
+import java.util.Calendar;
 import junit.framework.TestCase;
 
 /**
@@ -13,6 +14,21 @@ public class EnviaLembreteTest extends TestCase{
         Email email = new Email();  
         email.setNomeEmail("teste@hotmail.com");
         assertTrue(email.validaEmail() == true);
+    }
+    
+    public void testEnviarLembreteUmDiaAntes(){
+        
+        Calendar dataAtual    = Calendar.getInstance();
+        Calendar dataEsperada = Calendar.getInstance();
+        Email email           = new Email();
+        
+        //Representa a data e hora da atividade
+        dataAtual.set(2012,11,18 , 11,00,00);
+        //Atribuindo a tada e a hora
+        dataEsperada.set(2012,11,17 , 11,00,00);
+        
+        email.setDataAtual(dataAtual);
+        assertEquals(email.enviaUmDiaAntes(), dataEsperada);
     }
 }
 
