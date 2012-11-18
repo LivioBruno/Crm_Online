cb0c9e81ad9de3934766c476ed2c454a2d51c266
Livio Bruno <bruno_osprimos@hotmail.com>
2012-11-18 12:26:01 -0200

    Criada a classe Email e o metodo validaEmail()

---
+++ Email.java
@@ -0,0 +1,19 @@
+package Model;
+
+/**
+ *Class Email
+ * @author L.B.O
+ */
+public class Email {
+    
+    /**
+     * Método para validar email
+     * @return true caso email seja válido.
+     */
+    public boolean validaEmail(){
+        return true; 
+    }
+}
+
+
+
