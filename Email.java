d71bf1edfad0d2b1417c9bf08e5799de000e0824
Livio Bruno <bruno_osprimos@hotmail.com>
2012-11-18 12:57:56 -0200

    Refatorado o metodo validaEmail()

--- Email.java
+++ Email.java
@@ -1,17 +1,38 @@
 package Model;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  *Class Email
  * @author L.B.O
  */
 public class Email {
     
+    private String nomeEmail;
+
+    public String getNomeEmail() {
+        return nomeEmail;
+    }
+
+    public void setNomeEmail(String nomeEmail) {
+        this.nomeEmail = nomeEmail;
+    }
+    
     /**
      * Método para validar email
      * @return true caso email seja válido.
      */
     public boolean validaEmail(){
-        return true; 
+
+        Pattern p = Pattern.compile("^[\\w-]+(\\.[\\w-]+)*@([\\w-]+\\.)+[a-zA-Z]{2,7}$"); 
+        Matcher m = p.matcher(this.nomeEmail); 
+        if (m.find()){
+          return true;
+        }
+        else{
+          return false;
+        }  
     }
 }
 
