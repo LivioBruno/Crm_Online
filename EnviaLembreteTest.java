7bf5c3dd28b98a73cc5db96a980a2c6271f6a2b9
Livio Bruno <bruno_osprimos@hotmail.com>
2012-11-18 12:16:11 -0200

    Criada a classe que ira testar o envio de lembrete das atividades

---
+++ EnviaLembreteTest.java
@@ -0,0 +1,13 @@
+package TestClass;
+import junit.framework.TestCase;
+
+/**
+ * Classe para testar o envio de lembretes
+ * extende TestCase do JUNIT
+ * @author L.B.O
+ */
+public class EnviaLembreteTest extends TestCase{
+    
+    
+}
+
